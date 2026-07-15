34f8d32677add8887055d0ce7b74e1e072bb5149
Gerry Moeis <gerrymoeis@gmail.com>
2026-07-15 19:17:07 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.go
@@ -0,0 +1,19 @@
+func isValid(s string) bool {
+	stack := make([]byte, 0, len(s))
+	for i := range s {
+		switch s[i] {
+		case '(':
+			stack = append(stack, ')')
+		case '[':
+			stack = append(stack, ']')
+		case '{':
+			stack = append(stack, '}')
+		default:
+			if len(stack) == 0 || stack[len(stack)-1] != s[i] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
