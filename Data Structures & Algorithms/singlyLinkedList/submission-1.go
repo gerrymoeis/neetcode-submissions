14ad954a1ca1fca4c1170e10d5f71ab7f3f8fbc4
Gerry Moeis <gerrymoeis@gmail.com>
2026-07-18 16:33:41 +0700

    Update: singlyLinkedList - submission-1

--- Data Structures & Algorithms/singlyLinkedList/submission-1.go
+++ Data Structures & Algorithms/singlyLinkedList/submission-1.go
@@ -1,106 +1,90 @@
-type Node struct {
-	Val  int
-	Next *Node
-	Prev *Node
-}
+const Uninitialized = math.MinInt
 
 type LinkedList struct {
-	Head *Node
-	Tail *Node
-	Len  int
+	Val  int
+	Next *LinkedList
 }
 
 func NewLinkedList() *LinkedList {
-	return &LinkedList{}
+	return &LinkedList{Val: Uninitialized}
 }
 
-func (ll *LinkedList) Get(index int) int {
-	if index < 0 || index >= ll.Len {
-		return -1
-	}
-	var node *Node
-	if index < ll.Len/2 {
-		node = ll.Head
-		for i := 0; i < index; i++ {
+func (ll *LinkedList) GetNode(index int) *LinkedList {
+	node := ll
+	if index == -1 {
+		for node.Next != nil {
 			node = node.Next
 		}
 	} else {
-		node = ll.Tail
-		for i := ll.Len - 1; i > index; i-- {
-			node = node.Prev
+		for i := 0; i < index; i++ {
+			if node == nil {
+				break
+			}
+			node = node.Next
 		}
+	}
+	return node
+}
+
+func (ll *LinkedList) Get(index int) int {
+	if index < 0 || ll.Val == Uninitialized {
+		return -1
+	}
+	node := ll.GetNode(index)
+	if node == nil {
+		return -1
 	}
 	return node.Val
 }
 
 func (ll *LinkedList) InsertHead(val int) {
-	node := &Node{Val: val}
-	node.Next = ll.Head
-	if ll.Head != nil {
-		ll.Head.Prev = node
+	if ll.Val == Uninitialized {
+		ll.Val = val
+		return
 	}
-	ll.Head = node
-	ll.Len++
-	if ll.Tail == nil {
-		ll.Tail = node
-	}
+	newNode := &LinkedList{Val: ll.Val, Next: ll.Next}
+	ll.Val = val
+	ll.Next = newNode
 }
 
 func (ll *LinkedList) InsertTail(val int) {
-	node := &Node{Val: val}
-	if ll.Tail != nil {
-		ll.Tail.Next = node
-		node.Prev = ll.Tail
+	if ll.Val == Uninitialized {
+		ll.Val = val
+		return
 	}
-	ll.Tail = node
-	ll.Len++
-	if ll.Head == nil {
-		ll.Head = node
-	}
+	newNode := &LinkedList{Val: val}
+	node := ll.GetNode(-1)
+	node.Next = newNode
 }
 
 func (ll *LinkedList) Remove(index int) bool {
-	if index < 0 || index >= ll.Len {
+	if index < 0 || ll.Val == Uninitialized {
 		return false
 	}
-	node := ll.Head
-	switch index {
-	case 0:
-		ll.Head = node.Next
-		if ll.Head != nil {
-			ll.Head.Prev = nil
+	if index == 0 {
+		if ll.Next == nil {
+			ll.Val = Uninitialized
+		} else {
+			ll.Val = ll.Next.Val
+			ll.Next = ll.Next.Next
 		}
-	case ll.Len - 1:
-		node = ll.Tail
-		ll.Tail = node.Next
-	default:
-		for i := 0; i < index-1; i++ {
-			node = node.Next
+	} else {
+		prev := ll.GetNode(index - 1)
+		if prev == nil || prev.Next == nil {
+			return false
 		}
-		node.Next = node.Next.Next
-		if node.Next != nil {
-			node.Next.Prev = node
-		}
-	}
-	ll.Len--
-	if ll.Len == 0 {
-		ll.Tail = nil
+		prev.Next = prev.Next.Next
 	}
 	return true
 }
 
 func (ll *LinkedList) GetValues() []int {
-	values := make([]int, ll.Len)
-	node_1 := ll.Head
-	node_2 := ll.Tail
-	for i := 0; i < ll.Len/2; i++ {
-		values[i] = node_1.Val
-		node_1 = node_1.Next
-		values[ll.Len-1-i] = node_2.Val
-		node_2 = node_2.Prev
+	if ll.Val == Uninitialized {
+		return nil
 	}
-	if ll.Len%2 == 1 {
-		values[ll.Len/2] = node_1.Val
+	var values []int
+	for node := ll; node != nil; node = node.Next {
+		values = append(values, node.Val)
 	}
 	return values
 }
