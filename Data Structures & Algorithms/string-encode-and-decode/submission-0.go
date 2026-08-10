f201cd1bb39261b1ec813fd48a0bd786a14b4c27
Gerry Moeis <gerrymoeis@gmail.com>
2026-08-10 21:28:12 +0700

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.go
@@ -0,0 +1,26 @@
+const DIFF byte = 255
+
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	res := ""
+	for _, str := range strs {
+		res += str
+		res += string(DIFF)
+	}
+	return res
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	res := []string{}
+	str := ""
+	for _, c := range encoded {
+		if byte(c) == DIFF {
+			res = append(res, str)
+			str = ""
+			continue
+		}
+		str += string(c)
+	}
+	return res
+}
