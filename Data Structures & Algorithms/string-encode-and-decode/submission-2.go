8b017bebc7a2faa04d64945396f3cf160141788a
Gerry Moeis <gerrymoeis@gmail.com>
2026-08-12 12:42:41 +0700

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.go
@@ -0,0 +1,29 @@
+const DIFF byte = '/'
+
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	var res strings.Builder
+	for _, str := range strs {
+        res.WriteString(strconv.Itoa(len(str)))
+        res.WriteString(string(DIFF))
+        res.WriteString(str)
+	}
+	return res.String()
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	res := []string{}
+	i := 0
+    for i < len(encoded) {
+        j := i
+        for encoded[j] != DIFF {
+            j++
+        }
+        length, _ := strconv.Atoi(encoded[i:j])
+        i = j + 1
+        res = append(res, encoded[i:i+length])
+        i += length
+	}
+	return res
+}
