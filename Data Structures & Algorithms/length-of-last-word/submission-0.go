190e96246db59d9ed26a7c255e5eee24d470803a
Gerry Moeis <gerrymoeis@gmail.com>
2026-08-16 11:34:12 +0700

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.go
@@ -0,0 +1,12 @@
+func lengthOfLastWord(s string) int {
+	length := 0
+	for i := len(s)-1; i >= 0; i-- {
+		if length != 0 && s[i] == ' ' {
+			break
+		}
+		if s[i] != ' ' {
+			length++
+		}
+	}
+	return length
+}
