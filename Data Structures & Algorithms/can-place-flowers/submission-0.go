de5d134eaf9b041e4e3ee711be0075a771213790
Gerry Moeis <gerrymoeis@gmail.com>
2026-08-22 23:07:18 +0700

    Add: can-place-flowers - submission-0

---
+++ Data Structures & Algorithms/can-place-flowers/submission-0.go
@@ -0,0 +1,15 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == 0 {
+			if (len(flowerbed) == 1) ||
+				(i == 0 && flowerbed[i+1] == 0) ||
+				(i == len(flowerbed)-1 && flowerbed[i-1] == 0) ||
+				(i > 0 && i < len(flowerbed)-1 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0) {
+				flowerbed[i] = 1
+				n--
+				i++
+			}
+		}
+	}
+	return n <= 0
+}
